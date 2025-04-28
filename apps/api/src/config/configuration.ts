2db80b9763ed5751c4e8a6e1812ae6c6b5e3b551
Jarbit Lira <dev@jarbitlira.com>
2025-04-28 14:31:30 -0600

    Add sentry support to API

--- apps/api/src/config/configuration.ts
+++ apps/api/src/config/configuration.ts
@@ -1,5 +1,17 @@
+import * as Joi from 'joi';
+
 export default () => ({
-  port: parseInt(process.env.PORT, 10) || 3000,
+  validationSchema: Joi.object({
+    NODE_ENV: Joi.string()
+      .valid('development', 'production', 'test', 'provision')
+      .default('development'),
+    PORT: Joi.number().port().default(3000),
+  }),
+  validationOptions: {
+    allowUnknown: false,
+    abortEarly: true,
+  },
+
   database: {
     host: process.env.DB_HOST,
     port: parseInt(process.env.DB_PORT, 10) || 5432,
--- apps/api/src/instrument.ts
+++ apps/api/src/instrument.ts
@@ -6,6 +6,7 @@ const { nodeProfilingIntegration } = require('@sentry/profiling-node');
 // Ensure to call this before requiring any other modules!
 Sentry.init({
   dsn: process.env.SENTRY_DSN,
+  environment: process.env.NODE_ENV,
   sendDefaultPii: true,
   integrations: [
     // Add our Profiling integration
